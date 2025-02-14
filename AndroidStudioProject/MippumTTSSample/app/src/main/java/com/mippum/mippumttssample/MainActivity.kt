5a6008d35ea4808d9e4a7ab287d3327b8abb33ea
kei <wogud86@gmail.com>
2025-02-14 16:20:14 +0900

    feat: TTS speaks the sample text when a button is pushed.

--- AndroidStudioProject/MippumTTSSample/app/src/main/java/com/mippum/mippumttssample/MainActivity.kt
+++ AndroidStudioProject/MippumTTSSample/app/src/main/java/com/mippum/mippumttssample/MainActivity.kt
@@ -1,32 +1,60 @@
 package com.mippum.mippumttssample
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mippum.mippumttssample.ui.theme.MippumTTSSampleTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var textToSpeech: TextToSpeech
+    private lateinit var speakButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MippumTTSSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+        setContentView(R.layout.activity_main)
+
+        speakButton = findViewById(R.id.speak_button)
+
+        textToSpeech = TextToSpeech(this, OnInitListener { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val langResult = textToSpeech.setLanguage(Locale.US)
+                if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(this, "Language Missing", Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(this, "TTS Init Fail", Toast.LENGTH_SHORT).show()
             }
+        })
+
+        speakButton.setOnClickListener {
+            val textToRead = "Hello! This is TTS example."
+            speakOut(textToRead)
         }
+
     }
+
+    private fun speakOut(text: String) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+    }
+
+    override fun onDestroy() {
+        if (::textToSpeech.isInitialized) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        super.onDestroy()
+    }
+
 }
 
 @Composable
